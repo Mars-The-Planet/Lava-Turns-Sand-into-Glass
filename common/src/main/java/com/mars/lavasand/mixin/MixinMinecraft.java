28ca4f9694220d2c4701447cce6da9532b5466b9
Matej <ruzicka.maty@gmail.com>
2025-03-22 17:14:46 +0100

    first

---
+++ common/src/main/java/com/mars/lavasand/mixin/MixinMinecraft.java
@@ -0,0 +1,96 @@
+package com.mars.lavasand.mixin;
+
+import com.mars.lavasand.LavaSandConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Fallable;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(Block.class)
+public abstract class MixinMinecraft extends BlockBehaviour implements Fallable {
+    @Shadow private BlockState defaultBlockState;
+
+    @Shadow protected abstract Block asBlock();
+
+    public MixinMinecraft(Properties properties) {
+        super(properties);
+    }
+
+    public void onLand(Level p_52068_, BlockPos p_52069_, BlockState p_52070_, BlockState p_52071_, FallingBlockEntity p_52072_) {
+        if (shouldSolidify(p_52068_, p_52069_, p_52071_)) {
+            p_52068_.setBlock(p_52069_, Blocks.GLASS.defaultBlockState(), 3);
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "getStateForPlacement", cancellable = true)
+    public void getStateForPlacement(BlockPlaceContext context, CallbackInfoReturnable<BlockState> cir) {
+        BlockGetter blockgetter = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        BlockState blockstate = blockgetter.getBlockState(blockpos);
+        if(shouldSolidify(blockgetter, blockpos, this.defaultBlockState) || shouldSolidify(blockgetter, blockpos, blockstate)){
+            cir.setReturnValue(Blocks.GLASS.defaultBlockState());
+        }
+    }
+
+    private static boolean isOnSandList(Block block){
+        for (String sand : LavaSandConfig.blocks_to_glass){
+            try {
+                if(block.equals(Registry.BLOCK.get(new ResourceLocation(sand)))){
+                    return true;
+                }
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        return false;
+    }
+
+    private boolean shouldSolidify(BlockGetter p_52081_, BlockPos p_52082_, BlockState p_52083_) {
+        return (canSolidify(p_52083_) || touchesLiquid(p_52081_, p_52082_)) && isOnSandList(this.asBlock());
+    }
+
+    private static boolean touchesLiquid(BlockGetter p_52065_, BlockPos p_52066_) {
+        boolean flag = false;
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = p_52066_.mutable();
+
+        for(Direction direction : Direction.values()) {
+            BlockState blockstate = p_52065_.getBlockState(blockpos$mutableblockpos);
+            if (direction != Direction.DOWN || canSolidify(blockstate)) {
+                blockpos$mutableblockpos.setWithOffset(p_52066_, direction);
+                blockstate = p_52065_.getBlockState(blockpos$mutableblockpos);
+                if (canSolidify(blockstate) && !blockstate.isFaceSturdy(p_52065_, p_52066_, direction.getOpposite())) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        return flag;
+    }
+
+    private static boolean canSolidify(BlockState p_52089_) {
+        return p_52089_.getFluidState().is(FluidTags.LAVA);
+    }
+
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        return shouldSolidify(level, pos, state) ? Blocks.GLASS.defaultBlockState() : super.updateShape(state, direction, neighborState, level, pos, neighborPos);
+    }
+}
---
+++ fabric/src/main/java/com/mars/lavasand/LavaSand.java
@@ -0,0 +1,10 @@
+package com.mars.lavasand;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LavaSand implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
