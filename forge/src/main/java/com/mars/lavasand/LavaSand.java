0b857bbd4c625c3eb08211e496f1e392716aa80c
Matej <ruzicka.maty@gmail.com>
2025-03-22 17:03:15 +0100

    first

---
+++ forge/src/main/java/com/mars/lavasand/LavaSand.java
@@ -0,0 +1,10 @@
+package com.mars.lavasand;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class LavaSand {
+    public LavaSand() {
+        CommonClass.init();
+    }
+}
